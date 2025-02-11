e99b5aa4ba65c29170bb42fd35d30c708c094ce4
Arif <arif@themascoteers.com>
2025-02-11 12:49:59 +1100

    done

--- ndvi_progress_2.py
+++ ndvi_progress_2.py
@@ -1,30 +1,34 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 import matplotlib
 matplotlib.use('TkAgg')
 
-sites = ["site_1", "site_2", "site_3", "site_4"]
-file_paths = [f"central/{site}_ndvi_stats2.csv" for site in sites]
-data = [pd.read_csv(file, usecols=["year", "month", "avg_si"]) for file in file_paths]
+sis = ["ndvi","evi","savi",
+       "ndwi","pri","msavi",
+       "ndbi","ndii","chloro"
+       ]
 
-min_y = min(df["avg_si"].min() for df in data)
-max_y = max(df["avg_si"].max() for df in data)
+for si in sis:
+    sites = ["site_1", "site_2", "site_3", "site_4"]
+    file_paths = [f"central/{site}_{si}_stats2.csv" for site in sites]
+    data = [pd.read_csv(file, usecols=["year", "month", "avg_si"]) for file in file_paths]
 
-fig, axes = plt.subplots(4, len(sites), figsize=(4*len(sites), 22), sharey=True)
-fig.tight_layout(pad=3, h_pad=6)
+    min_y = min(df["avg_si"].min() for df in data)
+    max_y = max(df["avg_si"].max() for df in data)
 
-for i, year in enumerate(range(2021, 2025)):
-    for j, (df, site) in enumerate(zip(data, sites)):
-        ax = axes[i, j]
-        df_year = df[df["year"] == year]
-        ax.plot(df_year["month"], df_year["avg_si"], marker="o")
-        ax.set_xticks(range(1, 13))
-        ax.set_xticklabels(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], rotation=45)
-        ax.set_ylim(min_y, max_y)
-        ax.set_title(f"{site.replace('_', ' ').title()} - {year}")
-        if j == len(sites) // 2:
-            ax.tick_params(axis='y', labelleft=True)
+    fig, axes = plt.subplots(4, len(sites), figsize=(4*len(sites), 22), sharey=True)
+    fig.tight_layout(pad=3, h_pad=6)
 
-plt.savefig('plots/ndvi.png', dpi=300, bbox_inches='tight')
-#plt.show()
+    for i, year in enumerate(range(2021, 2025)):
+        for j, (df, site) in enumerate(zip(data, sites)):
+            ax = axes[i, j]
+            df_year = df[df["year"] == year]
+            ax.plot(df_year["month"], df_year["avg_si"], marker="o")
+            ax.set_xticks(range(1, 13))
+            ax.set_xticklabels(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], rotation=45)
+            ax.set_ylim(min_y, max_y)
+            ax.set_title(f"{site.replace('_', ' ').title()} - {year}")
+            if j == len(sites) // 2:
+                ax.tick_params(axis='y', labelleft=True)
+
+    plt.savefig(f'plots/{si}.png', dpi=300, bbox_inches='tight')
